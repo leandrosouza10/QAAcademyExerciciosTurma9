8c8aa802fd04984674975873b19f84acf34bfe8f
Leandro Souza <souzaleandro79@gmail.com>
2024-01-23 00:33:38 -0300

    Exercicio3Easy

--- src/Easy/Exercicio2.java
+++ src/Easy/Exercicio2.java
@@ -2,6 +2,15 @@ package Easy;
 
 import javax.swing.*;
 
+/* História de Usuário: Leitura e Exibição de Palavra
+        Como usuário, desejo ter a capacidade de inserir uma palavra e visualizá-la, permitindo uma interação simples
+        com um algoritmo de leitura e exibição.
+Critérios de Aceite:
+        ○ Como usuário, devo fornecer uma palavra como entrada para o algoritmo.
+        ○ O algoritmo deve ser capaz de ler e armazenar a palavra fornecida pelo usuário.
+        ○ O sistema deve exibir a palavra lida de forma clara e legível.
+        ○ O algoritmo deve permitir que o usuário insira múltiplas palavras para leitura */
+
 public class Exercicio2 {
 
     public static void main(String[] args) {
---
+++ src/Easy/Exercicio3.java
@@ -0,0 +1,26 @@
+package Easy;
+
+import javax.swing.*;
+
+/* História de Usuário: Troca de Valores
+        Como usuário, desejo ter a capacidade de fornecer dois valores, permitindo que um algoritmo os leia, armazene em variáveis
+        e os exiba trocados, a fim de facilitar a compreensão do funcionamento da troca de variáveis.
+   Critérios de Aceite:
+        ○ Como usuário, devo fornecer dois valores distintos como entrada para o algoritmo.
+        ○ O algoritmo deve ser capaz de ler os dois valores fornecidos pelo usuário e armazená-los em variáveis
+        distintas.
+        ○ O sistema deve exibir os valores trocados de forma clara e legível. */
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+
+        String valor1 = JOptionPane.showInputDialog("Informe um valor podendo ser número ou letras:");
+        String valor2 = JOptionPane.showInputDialog("Informe outro valor podendo ser número ou letras:");
+
+        System.out.println("Os valores informados foram: " + valor1 + " " + valor2 +
+                "\nAgora trocados: " + valor2 + " " + valor1);
+
+
+    }
+}
