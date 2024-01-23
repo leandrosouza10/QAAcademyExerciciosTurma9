d448db0c72b162fc555448b4e3ae3d6cb8419a2b
Leandro Souza <souzaleandro79@gmail.com>
2024-01-23 19:35:30 -0300

    Exercicio10Easy

---
+++ src/easy/Exercicio10.java
@@ -0,0 +1,41 @@
+package easy;
+
+import javax.swing.*;
+
+/*História de Usuário: Cálculo de Investimento com Juros Simples
+        Como usuário, desejo inserir um valor de investimento e visualizar, após 10 anos, o valor investido, o montante dos juros e
+        o total acumulado com juros, considerando uma taxa de juros simples de 12% ao ano. Isso proporcionará uma
+        compreensão clara do rendimento do investimento ao longo do tempo.
+ Critérios de Aceite:
+        ○ Como usuário, posso fornecer o valor do investimento como entrada para o algoritmo.
+        ○ O sistema deve calcular o montante dos juros simples ao longo de 10 anos, utilizando uma taxa de juros de
+        12% ao ano.
+        ○ O sistema deve calcular o total acumulado, somando o valor inicial do investimento e o montante dos juros
+        simples.
+        ○ O sistema deve exibir de forma clara e legível o valor investido, o montante dos juros e o total acumulado
+        com juros após 10 anos. */
+
+public class Exercicio10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("================== Cálculo de Investimento com Juros Simples =========================\n");
+
+        System.out.println("Fórmula para calcular juros simples: J = C × i × t \n" +
+                "J (juros) = C (capital inicial) x i (juros) x t (tempo)\nFórmula para descobrir a quantia final\n" +
+                "M = C + J \n" + "M = montante \n" +
+                "C = capital inicial \n" +
+                "J = juros (o resultado do cálculo anterior) ");
+        System.out.println("=========================================================================================\n");
+
+        double valorInvest = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do investimento R$: "));
+        double montanteJuros = valorInvest * 0.12 * 10;
+        double totalAcumulado = valorInvest + montanteJuros;
+
+        System.out.println("Valor do investimento R$ " + valorInvest);
+        System.out.println("Montante dos juros R$ " + montanteJuros);
+        System.out.println("Valor total acumulado com juros R$ " + totalAcumulado);
+
+    }
+
+}
