748c91522fce0777ddb1f10abf7291475d7eda1c
Leandro Souza <souzaleandro79@gmail.com>
2024-01-23 00:04:08 -0300

    Exercicio2Easy

---
+++ src/Easy/Exercicio2.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import javax.swing.*;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        String palavra = JOptionPane.showInputDialog("Digite uma palavra:");
+        System.out.println("A palavra digitada foi " + palavra);
+
+    }
+
+}
