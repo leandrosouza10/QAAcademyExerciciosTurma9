513738c196b341ce48047f63d52dbad4ea7ce1f1
Leandro Souza <souzaleandro79@gmail.com>
2024-01-23 02:02:01 -0300

    Inclusão de informações do exercicio

--- src/Easy/Exercicio6.java
+++ src/Easy/Exercicio6.java
@@ -2,6 +2,17 @@ package Easy;
 
 import javax.swing.*;
 
+   /* História de Usuário: Verificação de Aprovação em Disciplina
+            Como usuário, desejo ter a capacidade de inserir duas notas, permitindo que um algoritmo calcule a média. Após o cálculo, o sistema deve
+            determinar se a média é maior que 5, menor que 5 ou igual a 5, exibindo o status de aprovação, reprovação ou necessidade de exame.
+      Critérios de Aceite:
+            ○ Como usuário, posso fornecer duas notas como entrada para o algoritmo.
+            ○ O algoritmo deve calcular a média das duas notas fornecidas.
+            ○ Se a média for maior que 5, o sistema deve exibir "Aprovado".
+            ○ Se a média for menor que 5, o sistema deve exibir "Reprovado".
+            ○ Se a média for igual a 5, o sistema deve exibir "Exame".
+            ○ O sistema deve exibir de forma clara e legível o resultado da verificação de aprovação */
+
 public class Exercicio6 {
 
     public static void main(String[] args) {
