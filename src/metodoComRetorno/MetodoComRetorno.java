a6bd5f5e8a6573245d57c1c0fb478ac4bfbcabb9
Leandro Souza <souzaleandro79@gmail.com>
2024-03-02 00:51:19 -0300

    Método com retorno

--- src/metodoComRetorno/MetodoComRetorno.java
+++ src/metodoComRetorno/MetodoComRetorno.java
@@ -1,5 +1,50 @@
 package metodoComRetorno;
 
+/*
+    1- Criar uma classe;
+    2- Criar 5 metodos com retorno:
+    3- Adicionar uma instrução para retornar o nome do Método;
+    4- Fazer a chamada destes 5 métodos em um main.
+*/
+
 public class MetodoComRetorno {
+
+    public static String notaFinal() {
+        int nota = 8;
+
+        if (nota >= 7) {
+            return "Aprovado " + nota;
+        } else {
+            return "Reprovado " + nota;
+        }
+
+    }
+
+    public static String retornoDois() {
+        return "MÉTODO DOIS";
+    }
+
+    public static String retornoTres() {
+        return "MÉTODO TRÊS";
+    }
+
+    public static String retornoQuatro() {
+        return "MÉTODO QUATRO";
+    }
+
+    public static String retornoCinco() {
+        return "MÉTODO CINCO";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(notaFinal());
+        System.out.println(retornoDois());
+        System.out.println(retornoTres());
+        System.out.println(retornoQuatro());
+        System.out.println(retornoCinco());
+
+    }
+
 
 }
